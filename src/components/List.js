0c756272412d44b2247a56442777d124f2bdca51
Cimorexave <sadeq.s502@gmail.com>
2022-06-04 13:51:01 +0430

    completed stylings and mobile responsive

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import { v4 as uuidv4 } from 'uuid';
+import './list.css'
 
 const List = ({notes }) => {
   console.log('notes are: ' ,notes)
   return (
     <>
+    <p className='title'> Notes </p>
         {notes.map((item) => { return (
-            <div key={uuidv4()}>
+            <div className='item' key={uuidv4()}>
               {item}
             </div>
         )})}
